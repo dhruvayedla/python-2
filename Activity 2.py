e52a72bfa5a0e9f914f30d2a5af0d589583ab504
dhruvayedla <ysreddy@outlook.com>
2024-11-13 19:03:55 +0000

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,43 @@
+#Write a program for print command.    # are used to give comments
+
+print("Druva!! Welcome to the world of coding!")
+print('hshshs')
+print(1)
+print("edfgyuhjikoldfbngygherfbv sfher")
+print('welcome to the world of coding')
+
+
+#Write a program to create different variables to store various values.    
+ 
+
+x="Druva"
+print(x)
+print("x")
+
+y=0
+print(y)
+g='dhruva'
+print(g)
+m=1
+print(m)
+c="England"
+print(c)
+
+country="INDIA"
+print(country)
+
+dob="13 April 2000"
+
+print(dob)
+
+fc="Manchester united"
+print(fc)
+
+qwqwerwet=357842454
+print(qwqwerwet)
+
+#Write a program to print all the keywords
+
+import keyword
+print("the keyword in python are..\n")
+print(keyword.kwlist)
